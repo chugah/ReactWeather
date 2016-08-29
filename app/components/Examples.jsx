a472bd9bc69d9676e953e40825d87b405259a17f
Sami Barghshoon <chugah@users.noreply.github.com>
2016-08-29 14:45:02 -0400

    Style web pages

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,21 +1,17 @@
 var React = require('react');
-
-// Refactored below
-// var Examples = React.createClass({
-// 	render: function () {
-// 		return (
-//    			<h3>Examples component</h3>
-// 		);
-// 	}
-// });
+var {Link} = require('react-router');
 
 var Examples = (props) => {
 	return (
-   			<div>
-   				<h3>Examples</h3>
-   				<p>Welcome to Examples page.</p>
-   			</div>
-		)
+   		<div>
+   			<h1 className="text-center">Examples</h1>
+   			<p>Here are a few example locations to try out:</p>
+   			<ol>
+   				<li><Link to='/?location=Philadelphia'>Philadelphia, PA</Link></li>
+   				<li><Link to='/?location=Rio'>Rio, Brazil</Link></li>
+   			</ol>
+   		</div>
+	)
 };
 
 module.exports = Examples;
