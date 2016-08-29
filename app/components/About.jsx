efa9946bbbf904947a43f0796d047e9f594441b2
Sami Barghshoon <chugah@users.noreply.github.com>
2016-08-29 15:10:39 -0400

    Style added to About page & link to resources provided

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,22 +1,17 @@
 var React = require('react');
 
-
-//Refactored below
-// var About = React.createClass({
-// 	render: function () {
-// 		return (
-//    			<h3>About component</h3>
-// 		);
-// 	}
-// });
-
 var About = (props) => {
 	return (
-			<div>
-   				<h3>About</h3>
-   				<p>Welcome to the About page.</p>
-   			</div>
-		)
+		<div>
+   			<h1 className="text-center">About</h1>
+   			<p>This is a weather application built using React for a web developer course.</p>
+   			<p> Here are some of the tools that were used:</p>
+	   		<ul>
+	   			<li><a href="https://facebook.github.io/react" target="blank">React</a> - This was the Javascript framework used.</li>
+	   			<li><a href="http://openweathermap.org" target="blank">Open Weather Map</a> -  API used to get weather data by city.</li>
+	   		</ul>
+   		</div>
+	)
 };
 
 module.exports = About;
